889bc86dc86af40ced135761b8475aa87201ad27
3xer <pontuslundholm1@gmail.com>
2023-03-08 14:11:54 +0100

    all

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from "express"
+
+const app = express();
+const PORT = process.env.PORT || 3080;
+
+app.get('/', (req, res) => {res.send ('Yes, it works!');});
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
